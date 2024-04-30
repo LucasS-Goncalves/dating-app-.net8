62d74b5e75bf1da45b3c9434c8d748c3d92b73cd
Lucas <lucasgoncalves08@live.com>
2024-04-30 16:45:09 -0300

    end of section 14

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -32,6 +32,12 @@ namespace DatingApp.Entities
 
         public string Interests { get; set; }
 
-        public List<Photo> Photos { get; set; } = new();       
+        public List<Photo> Photos { get; set; } = new();
+
+        public List<UserLike> LikedByUsers { get; set; }
+
+        public List<UserLike> LikedUsers { get; set; }
+
+
     }
 }
---
+++ Interfaces/ILikesRepository.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.DTOs;
+using DatingApp.Entities;
+using DatingApp.Helpers;
+
+namespace DatingApp.Interfaces
+{
+    public interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+
+        Task<AppUser> GetUserWithLikes(int userId);
+
+        Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams);
+    }
+}
